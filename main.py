db673e3a41e11e360774d3368b3d4a2102f4122b
Guilherme Yamada <yamadadan@hotmail.com>
2022-10-24 22:50:51 -0300

    First commit

--- main.py
+++ main.py
@@ -1,36 +1,14 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
-from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
-CORS(app)
 
-# class HelloWorld(Resource):
-#     def get(self):
-#         return {'hello': 'world'}
-
-class status(Resource):    
-     def get(self):
-         try:
-            return {'data': 'Api running'}
-         except: 
-            return {'data': 'An Error Occurred during fetching Api'}
-
-class Sum(Resource):
-    def get(self, a, b):
-        return jsonify({'data': a+b})
-
-
-class Soma(Resource):
+class HelloWorld(Resource):
     def get(self):
-        return {'soma':1+2}
+        return {'hello': 'world'}
 
-# api.add_resource(HelloWorld, '/')
-api.add_resource(status,'/')
-api.add_resource(Sum,'/add/<int:a>,<int:b>')
-api.add_resource(Soma, '/soma/')
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run()
+    app.run(debug=True)
